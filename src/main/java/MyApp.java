ab4bb6cbf30e8ed590603daf9ee5195dfb38d73d
Bogdan Bardakov <bogdan_bardakov@epam.com>
2017-04-18 14:32:12 +0300

    webbit embedded

---
+++ src/main/java/MyApp.java
@@ -0,0 +1,15 @@
+import org.webbitserver.WebServer;
+import org.webbitserver.netty.NettyWebServer;
+
+public class MyApp {
+    private static final int PORT = 2017;
+    private static final int MILLIS = 1000;
+
+    public static void main(String[] args) throws Exception {
+        WebServer webServer = new NettyWebServer(PORT)
+                .add(new SimpleServerHandler())
+                .staleConnectionTimeout(MILLIS);
+        webServer.start().get();
+        System.out.println("Listening on " + webServer.getUri());
+    }
+}
---
+++ src/main/java/SimpleServerHandler.java
@@ -0,0 +1,36 @@
+import org.webbitserver.HttpControl;
+import org.webbitserver.HttpHandler;
+import org.webbitserver.HttpRequest;
+import org.webbitserver.HttpResponse;
+
+import java.text.NumberFormat;
+
+class SimpleServerHandler implements HttpHandler {
+    public void handleHttpRequest(HttpRequest httpRequest, HttpResponse httpResponse, HttpControl httpControl) throws Exception
+    {
+        httpResponse.header("Content-type", "text/html")
+                .content("<html>" +
+                        "   <body>" +
+                            memoryUsage() +
+                        "   </body>" +
+                        "</html>")
+                .end();
+    }
+
+    private String memoryUsage() {
+        Runtime runtime = Runtime.getRuntime();
+
+        NumberFormat format = NumberFormat.getInstance();
+
+        StringBuilder sb = new StringBuilder();
+        long maxMemory = runtime.maxMemory();
+        long allocatedMemory = runtime.totalMemory();
+        long freeMemory = runtime.freeMemory();
+
+        sb.append("free memory: ").append(format.format(freeMemory / 1024)).append("<br/>");
+        sb.append("allocated memory: ").append(format.format(allocatedMemory / 1024)).append("<br/>");
+        sb.append("max memory: ").append(format.format(maxMemory / 1024)).append("<br/>");
+        sb.append("total free memory: ").append(format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024)).append("<br/>");
+        return sb.toString();
+    }
+}
